4e53da1e86baba08db618d4ad719117d376d10b8
Marcell Nagy <marcika863@gmail.com>
2023-05-24 10:06:39 +0200

    4.feladat kész

--- Program.cs
+++ Program.cs
@@ -16,8 +16,20 @@ namespace ConsoleApp16
             File.ReadLines("DataSource\\kifejezesek.txt")
             .Select(x => new Kifejezesek(x))
             .ToList();
-            
-
+            //2
+            Console.WriteLine($"2.feladat: Kifejezések száma: {kifej.Count()}");
+            //3
+            Console.WriteLine($"3.feladat: Kifejezések száma maradékos osztással:{kifej.Count(x=> x.OperStr == "mod")}");
+            //4
+            var vanE = kifej.Any(x => x.FirstNum % 10 == 0 && x.SecondNum % 10 == 0);
+            if (vanE)
+            {
+                Console.WriteLine("4.feladat: Van ilyen kifejezés");
+            }
+            else
+            {
+                Console.WriteLine("4.feladat: Nincs ilyen kifejezés");
+            }
         }
     }
 }
